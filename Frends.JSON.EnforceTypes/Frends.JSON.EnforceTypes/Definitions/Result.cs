27f771345ee8069cca26433bb6644d55c55d368f
Riku Virtanen <virtanen.riku91@gmail.com>
2022-04-05 15:09:49 +0300

    Added documentation to Result class

--- Frends.JSON.EnforceTypes/Frends.JSON.EnforceTypes/Definitions/Result.cs
+++ Frends.JSON.EnforceTypes/Frends.JSON.EnforceTypes/Definitions/Result.cs
@@ -4,8 +4,14 @@
 
 namespace Frends.JSON.EnforceTypes.Definitions
 {
+    /// <summary>
+    /// Return object with private setters
+    /// </summary>
     public class Result
     {
+        /// <summary>
+        /// The input json as a string.
+        /// </summary>
         public string JsonAsString { get; private set; }
 
         public Result(string json)

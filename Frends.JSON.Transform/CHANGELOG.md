c8976472eba059681148ea3438cb1856fe62c33b
Riku Virtanen <virtanen.riku91@gmail.com>
2022-10-31 16:44:33 +0200

    Fixed issue 7 and modified input fields, changed result to dynamic

--- Frends.JSON.Transform/CHANGELOG.md
+++ Frends.JSON.Transform/CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## [1.0.1] - 2022-10-31
+### Fixed
+- Changed the Result JToken to be dynamic to enable dot notation
+- Changed input parameter field default DataFormatString to Json
+- Updated documentation
+- Fixed tests
+
 ## [1.0.0] - 2022-04-06
 ### Added
 - Initial implementation
--- Frends.JSON.Transform/Frends.JSON.Transform.Tests/UnitTests.cs
+++ Frends.JSON.Transform/Frends.JSON.Transform.Tests/UnitTests.cs
@@ -3,122 +3,124 @@ using System;
 using Newtonsoft.Json.Linq;
 using Frends.JSON.Transform.Definitions;
 
-namespace Frends.JSON.Transform.Tests
+namespace Frends.JSON.Transform.Tests;
+
+[TestFixture]
+class TestClass
 {
-    [TestFixture]
-    class TestClass
+    Input _testInput;
+    private const string _testJson =
+@"
+{
+""firstName"": ""Veijo"",
+""lastName"": ""Frends"",
+""age"": 30,
+""retired"": false
+}
+";
+    private const string _testMap =
+@"
+{
+""FullName"": ""#xconcat(#valueof($.firstName), ,#valueof($.lastName))"",
+""Age"" : ""#valueof($.age)"",
+""StillBreething"": ""#valueof($.retired)""
+}
+";
+    [SetUp]
+    public void TestSetup()
     {
-        Input _testInput;
-        private const string _testJson =
-@"
-{
-    ""firstName"": ""Veijo"",
-    ""lastName"": ""Frends"",
-    ""age"": 30,
-    ""retired"": false
-}
-";
-        private const string _testMap =
-@"
-{
-    ""FullName"": ""#xconcat(#valueof($.firstName), ,#valueof($.lastName))"",
-    ""Age"" : ""#valueof($.age)"",
-    ""StillBreething"": ""#valueof($.retired)""
-}
-";
-        [SetUp]
-        public void TestSetup()
+        _testInput = new Input
         {
-            _testInput = new Input
-            {
-                InputJson = _testJson,
-                JsonMap = _testMap
-            };
-        }
+            InputJson = _testJson,
+            JsonMap = _testMap
+        };
+    }
 
-        [Test]
-        public void TransformShouldAllowJTokenAsInput()
-        {
-            _testInput.InputJson = JToken.Parse(_testJson);
-            var result = JSON.Transform(_testInput);
-        }
+    [Test]
+    public void TransformShouldAllowJTokenAsInput()
+    {
+        _testInput.InputJson = JToken.Parse(_testJson);
+        var result = JSON.Transform(_testInput);
+        Assert.AreEqual("{\"FullName\":\"Veijo Frends\",\"Age\":30,\"StillBreething\":false}", result.Transformation);
+    }
 
-        [Test]
-        public void TransformShouldAllowStringAsInput()
-        {
-            var result = JSON.Transform(_testInput);
-        }
+    [Test]
+    public void TransformShouldAllowStringAsInput()
+    {
+        var result = JSON.Transform(_testInput);
+        Assert.AreEqual("{\"FullName\":\"Veijo Frends\",\"Age\":30,\"StillBreething\":false}", result.Transformation);
+    }
 
-        [Test]
-        public void TransformMapsStringData()
-        {
-            var result = JSON.Transform(_testInput);
+    [Test]
+    public void TransformMapsStringData()
+    {
+        var result = JSON.Transform(_testInput);
 
-            var fullName = result.JToken["FullName"].Value<string>();
+        var fullName = result.JToken.FullName;
 
-            Assert.AreEqual("Veijo Frends", fullName);
-        }
+        Assert.AreEqual("Veijo Frends", fullName.ToString());
+    }
 
-        [Test]
-        public void TransformMapsNumbersCorrectly()
-        {
-            var result = JSON.Transform(_testInput);
+    [Test]
+    public void TransformMapsNumbersCorrectly()
+    {
+        var result = JSON.Transform(_testInput);
 
-            var age = result.JToken["Age"];
+        var age = result.JToken.Age;
 
-            Assert.AreEqual(JTokenType.Integer, age.Type);
-            Assert.AreEqual(30, age.Value<int>());
-        }
+        Assert.AreEqual(JTokenType.Integer, age.Type);
+        Assert.AreEqual(30, (int)age);
+    }
 
-        [Test]
-        public void TransformationMapsBoolValueCorrectly()
-        {
-            var result = JSON.Transform(_testInput);
+    [Test]
+    public void TransformationMapsBoolValueCorrectly()
+    {
+        var result = JSON.Transform(_testInput);
 
-            var breething = result.JToken["StillBreething"];
+        var breething = result.JToken.StillBreething;
 
-            Assert.AreEqual(JTokenType.Boolean, breething.Type);
-            Assert.AreEqual(false, breething.Value<bool>());
-        }
+        Assert.AreEqual(JTokenType.Boolean, breething.Type);
+        Assert.AreEqual(false, (bool)breething);
+    }
 
-        [Test]
-        public void TransformWorksWithArray()
-        {
-            _testInput.InputJson = @"{ ""array"": [{""key"":""first element""},{""key"":""second element""}]}";
-            _testInput.JsonMap = @"{""firstElement"":""#valueof($.array[0].key)""}";
+    [Test]
+    public void TransformWorksWithArray()
+    {
+        _testInput.InputJson = @"{ ""array"": [{""key"":""first element""},{""key"":""second element""}]}";
+        _testInput.JsonMap = @"{""firstElement"":""#valueof($.array[0].key)""}";
 
-            var result = JSON.Transform(_testInput);
-            var firstElement = result.JToken["firstElement"].Value<string>();
+        var result = JSON.Transform(_testInput);
+        var firstElement = result.JToken.firstElement;
 
-            Assert.AreEqual("first element", firstElement);
+        Assert.AreEqual("first element", (string)firstElement);
             
-        }
+    }
 
-        [Test]
-        public void TransformThrowsWithArrayRootElement()
-        {
-            _testInput.InputJson = @"[{""key"": ""first element""}, {""key"": ""second element""}]";
-            _testInput.JsonMap = @"{""firstElement"": ""#valueof($.[0].key)""}";
+    [Test]
+    public void TransformThrowsWithArrayRootElement()
+    {
+        _testInput.InputJson = @"[{""key"": ""first element""}, {""key"": ""second element""}]";
+        _testInput.JsonMap = @"{""firstElement"": ""#valueof($.[0].key)""}";
 
-            var ex = Assert.Throws<Exception>(() => JSON.Transform(_testInput));
-            Assert.That(ex.Message.Equals("Json transformation failed: Input Json is not valid: Array is not supported as root element."));
-        }
+        var ex = Assert.Throws<Exception>(() => JSON.Transform(_testInput));
+        Assert.That(ex.Message.Equals("Json transformation failed: Input Json is not valid: Array is not supported as root element."));
+    }
 
 
-        [Test]
-        public void InvalidJsonInputThrowsException()
-        {
-            _testInput.InputJson = @"{ foo baar";
+    [Test]
+    public void InvalidJsonInputThrowsException()
+    {
+        _testInput.InputJson = @"{ foo baar";
 
-            Assert.Throws<FormatException>(() => JSON.Transform(_testInput));
-        }
+        Assert.Throws<FormatException>(() => JSON.Transform(_testInput));
+    }
 
-        [Test]
-        public void InvalidJsonMapThrowsException()
-        {
-            _testInput.JsonMap = @"{""age"":""#valuof($.age)"", ""foo}";
+    [Test]
+    public void InvalidJsonMapThrowsException()
+    {
+        _testInput.JsonMap = @"{""age"":""#valuof($.age)"", ""foo}";
 
-            Assert.Throws<Exception>(() => JSON.Transform(_testInput));
-        }
+        Assert.Throws<Exception>(() => JSON.Transform(_testInput));
     }
 }
+
